af77ea452b23130d87eafb27d3550ea6295cf893
planetoryd <planetoryd>
2022-11-13 20:28:03 +0800

    somewhat usable

--- offdict-tauri/src-tauri/src/main.rs
+++ offdict-tauri/src-tauri/src/main.rs
@@ -2,20 +2,97 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-use offdictd::{self};
+use config::{Config, File, FileFormat};
+use offdictd::{self, *};
+use std::{borrow::Borrow, env, path::PathBuf, sync::Arc, sync::RwLock};
+use tauri::Manager;
+
+#[derive(Debug, Deserialize)]
+struct OffdictConfig {
+    data_path: String,
+}
+
+pub struct InnerState<'a> {
+    pub db: Option<DB>,
+    pub trie: Option<FuzzyTrie<'a, String>>,
+    pub trie_buf: Vec<u8>,
+}
+
+pub struct OffdictState<'a>(pub RwLock<InnerState<'a>>);
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
+fn candidates<'a>(
+    state: tauri::State<'a, OffdictState>,
+    query: &'a str,
+) -> Result<Vec<String>, ()> {
+    // offdictd::candidates(trie, query, num_max)
+    let state_guard = state.0.read().unwrap();
+    // Change field of state struct
+    // Replace state struct; here you need to dereference the guard to get the pointer to the inner value (I think)
+    // *state_guard = InnerState {
+    //     db: None,
+    //     trie: None,
+    // };
+    let strs = offdictd::candidates(state_guard.trie.as_ref().unwrap(), query, 5);
+    println!("{:?}", strs);
 
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    Ok(strs.into_iter().map(|x| x.clone()).collect()) // lets clone it bc idk how to solve it atm
 }
 
+#[tauri::command]
+fn defs<'a>(state: tauri::State<'a, OffdictState>, query: &'a str) -> Result<Def, &'static str> {
+    let state_guard = state.0.read().unwrap();
 
+    let d = offdictd::search_single(
+        state_guard.db.as_ref().unwrap(),
+        state_guard.trie.as_ref().unwrap(),
+        query,
+    );
+
+    match d {
+        Some(de) => Ok(de),
+        None => Err("not found"),
+    }
+}
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            println!("{}", env::current_dir().unwrap().to_str().unwrap());
+            let config = Config::builder()
+                .set_default("data_path", ".")
+                .unwrap()
+                .add_source(File::new("config", FileFormat::Json5))
+                .build()
+                .unwrap();
+
+            let conf: OffdictConfig = config.try_deserialize().unwrap();
+            println!("{:?}", conf);
+            let window = app.get_window("main").unwrap();
+            // window.emit("error", 1);
+            let state: tauri::State<OffdictState> = app.state();
+            let mut state_guard = state.0.write().unwrap();
+
+            let mut db_path = PathBuf::from(conf.data_path.clone());
+            db_path.push("rocks_t");
+            let mut trie_path = PathBuf::from(conf.data_path.clone());
+            trie_path.push("trie");
+
+            state_guard.db = Some(open_db(db_path.to_str().unwrap()));
+
+            state_guard.trie = Some(load_trie(
+                trie_path.to_str().unwrap(),
+                &mut state_guard.trie_buf,
+            ));
+            Ok(())
+        })
+        .manage(OffdictState(RwLock::new(InnerState {
+            db: None,
+            trie: None,
+            trie_buf: Vec::new(),
+        })))
+        .invoke_handler(tauri::generate_handler![candidates, defs])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ offdict-tauri/src/lib/DefUnit.svelte
@@ -0,0 +1,103 @@
+<script>
+  export let def;
+</script>
+
+<div class="explain">
+  {#if def.CN}
+    <div class="CN">{def.CN}</div>
+  {/if}
+  {#if def.EN}
+    <div class="EN">{def.EN}</div>
+  {/if}
+  {#if def.examples}
+    {#each def.examples as ex}
+      <div class="example">
+        {#if ex.CN}
+          <div>{ex.CN}</div>
+        {/if}
+        {#if ex.EN}
+          <div>{ex.EN}</div>
+        {/if}
+        {#if typeof ex === "string"}
+          <div>{ex}</div>
+        {/if}
+      </div>
+    {/each}
+  {/if}
+  {#if def.type}
+    <div class="chip">{def.type}</div>
+  {/if}
+  {#if def.info}
+    <div class="info">{def.info}</div>
+  {/if}
+  {#if def.related}
+    {#each def.related as re}
+      <div class="related">
+        {#if re.CN}
+          <div>{re.CN}</div>
+        {/if}
+        {#if re.EN}
+          <div>{re.EN}</div>
+        {/if}
+        {#if typeof re === "string"}
+          <div>{re}</div>
+        {/if}
+      </div>
+    {/each}
+  {/if}
+  <div class="unknown">
+    {#if def.definitions}
+      {#if def.title}
+        <div class="titl">{def.title}</div>
+      {/if}
+
+      {#each def.definitions as d}
+        {#if d.CN}
+          <div class="CN">{d.CN}</div>
+        {/if}
+        {#if d.EN}
+          <div class="EN">{d.EN}</div>
+        {/if}
+      {/each}
+    {/if}
+  </div>
+</div>
+
+<style lang="scss">
+  .example {
+    margin-left: 20px;
+    margin-top: 5px;
+  }
+
+  .unknown {
+    .CN,
+    .EN {
+      background: white;
+      color: #868e96;
+    }
+  }
+
+  .CN,
+  .EN,
+  .titl {
+    color: #f1f3f5;
+    background: #868e96d2;
+    padding: 4px;
+    // border-radius: 2px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    padding-left: 15px;
+    padding-right: 10px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+  }
+  .info {
+    color: #868e96;
+  }
+  .explain {
+    margin-bottom: 10px;
+  }
+  .related {
+    margin-top: 15px;
+  }
+</style>
